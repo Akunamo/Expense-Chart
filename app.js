f14c01ebff8ac8bb39ce3e04209c64cb8a0672a5
Aviral <2knightsoldier@gmail.com>
2022-12-11 15:21:19 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,59 @@
+// const expenseChart = document.querySelector('.expense-chart');
+
+// let spent = []
+// let html = document.createElement('div');
+// fetch('/data.json')
+//     .then(data => data.json())
+//     .then(data => {
+//         data.forEach(exp => {
+//             data = `<div><span class='expense-bar' data-value='${exp.amount}'></span><span>${exp.day}</span></div>`
+//             spent.push(data)
+//             html.append(data);
+//         });
+//         // expenseChart.textContent = html;
+//         console.log(html);
+//     })
+
+
+    // Using innerText
+
+const expenseChart = document.querySelector('.expense-chart');
+
+let spent = []
+fetch('/data.json')
+    .then(data => data.json())
+    .then(data => {
+        data.forEach(exp => {
+            data = `<div class="day"><div class='expense-bar' data-value='${exp.amount}'><p class="amount-day text-white">$${exp.amount}</p></div><p>${exp.day}</p></div>`
+            spent.push(data)
+        });
+        expenseChart.innerHTML = spent.join('');
+        animateChart()
+    })
+
+function animateChart(){
+    let expenses = expenseChart.querySelectorAll('.expense-bar');
+    let max = 0;
+    expenses.forEach(e => {
+        let amount = e.getAttribute('data-value')
+        if(max < amount) max = amount;
+    })
+    setTimeout(_ => {
+        setHeight(max,expenses)
+    },1000)
+    console.log(max);
+}
+
+function setHeight(high,arr) {
+    let sum = []
+    arr.forEach((a,i)=> {
+        let amount = parseFloat(a.getAttribute('data-value'))
+        // console.log(amount*50/45,Math.round(amount*100/high));
+        a.style.height = Math.round(amount*200/high) + 'px';
+        a.style.transitionDelay = i*100 +'ms';
+        a.classList.add('active')
+        if(amount === high) a.style.backgroundColor = 'var(--Cyan)'
+        sum.push(amount);
+    })
+    console.log(sum.reduce((a,i) => a+i));
+}
